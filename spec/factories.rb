0b221f7fd212a053cadaf24f73f6f5a787418ba5
knvkn <v.knight@ukr.net>
2014-05-20 23:31:03 +0300

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Vitaly Kravchenko"
+    email    "knightnya@gmail.com"
+    password "acdc666"
+    password_confirmation "acdc666"
+  end
+end
